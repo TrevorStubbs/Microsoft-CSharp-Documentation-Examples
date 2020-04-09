18823889df1735c29758355429782cce26ce6f74
Trevor Stubbs <stubbste@gmail.com>
2020-04-09 12:04:59 -0700

    Added CalculationDLL

---
+++ CalculationDLL/Calculation.cs
@@ -0,0 +1,20 @@
+﻿/* Code Based off of:
+ * https://www.c-sharpcorner.com/UploadFile/1e050f/creating-and-using-dll-class-library-in-C-Sharp/
+*/
+
+using System;
+
+namespace Calculation
+{
+    public class Calculate
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}

24131f0d4d84aaabb45c61566365dcfc7774e4b9
Trevor Stubbs <stubbste@gmail.com>
2020-04-20 10:29:34 -0700

    added interface example

---
+++ InterfaceType/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+  Interface Type: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/interface
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceType
+{
+    interface IPoint
+    {
+        // Property signatures:
+        int X
+        {
+            get;
+            set;
+        }
+
+        int Y
+        {
+            get;
+            set;
+        }
+
+        double Distance
+        {
+            get;
+        }
+    }
+
+    class Point : IPoint
+    {
+        // Constructor:
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        // Property implementation:
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        // Property implementation
+        public double Distance =>
+           Math.Sqrt(X * X + Y * Y);
+
+    }
+
+    class MainClass
+    {
+        static void PrintPoint(IPoint p)
+        {
+            Console.WriteLine("x={0}, y={1}", p.X, p.Y);
+        }
+
+        static void Main()
+        {
+            IPoint p = new Point(2, 3);
+            Console.Write("My Point: ");
+            PrintPoint(p);
+            Console.ReadKey();
+        }
+    }
+}
+// Output: My Point: x=2, y=3
---
+++ InterfaceType/readme.md
@@ -0,0 +1,31 @@
+# [Interface Type](https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/interface)
+
+# What is an Interface
+- An interface defines a contract.
+- It's a collection of method and property declarations.
+- Any `class` or `struct` that implements an interface must provide implementation for the defined methods and properties.
+
+# Interface Naming
+- use the `interface` keyword then `I[Name]` - ie 
+```C#
+interface IPoint
+```
+
+# Interface inheritance rules
+- An interface can inherit from any number of interfaces 
+```C#
+interface ICoord : IPoint
+```
+- It can not inherit from a class at all ~~`interface ICoord : Point`~~
+
+# Using an interface while declaring a class
+- use `:` to inherit the interface contract
+```C#
+class Point : IPoint
+```
+- a class can inherit from any number of interfaces
+```C#
+class Coord : ICoord : IPoint
+```
+
+
